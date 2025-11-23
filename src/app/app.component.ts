4a4b87f7e8581f70ab9e38002246ec1166d363fa
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-11-23 20:37:02 +0530

    Removed unwanted imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { ProductsListComponent } from './pages/products-list/products-list.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProductsListComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet],
   template: `<app-header />
     <router-outlet /> `,
   styles: [],
